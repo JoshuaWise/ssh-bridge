e584b40bcd3097e83f80ace0e615e4eb7142275b
Joshua Wise <joshuathomaswise@gmail.com>
2025-07-13 04:19:19 -0500

    fixed escapeShell() for Windows

--- test/tools/shell-escape.js
+++ test/tools/shell-escape.js
@@ -1,12 +1,16 @@
 'use strict';
+const WIN32 = process.platform === 'win32';
 
 module.exports = (...args) => {
 	return args.map(String).map(escapeShellArg).join(' ');
 };
 
-function escapeShellArg(arg) {
-	if (process.platform === 'win32') {
-		return `"${arg.replace(/(["\\])/g, '\\$1')}"`;
+function escapeShellArg(arg, index) {
+	if (WIN32) {
+		if (index > 0) { // On Windows, only quote args after the command name
+			arg = `"${arg.replace(/(\\+)("|$)/g, '$1$1$2').replace(/"/g, '\\"')}"`;
+		}
+		return arg.replace(/[()%!^"<>&|;, ]/g, '^$&');
 	} else {
 		return `'${arg.replace(/'/g, '\'\\\'\'')}'`;
 	}
